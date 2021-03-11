338ad33e21e212b178ab5190dcbe7ee86cfca276
Guilherme Vieira <guilhermevmelo@gmail.com>
2021-03-10 22:15:52 -0300

    Add validation to the psionic power

--- telekinetic-force.js
+++ telekinetic-force.js
@@ -1,26 +1,71 @@
 const psion = game.user.character
 
-let maxPsiPointsExpendableAtOnce = Math.ceil(psion.data.data.details.level / 2)
+let psiPointsPerUseLimit = Math.ceil(psion.data.data.details.level / 2)
 
 class TelekineticForce {
-    constructor(target, hammering = 0, crushing = 0, zoneOf = 0, hurling = 0, hurlingDirections = []) {
-        this.target = target
-        
+    /**
+     * 
+     * @param {Set} targets 
+     * @param {Number} hammering 
+     * @param {Boolean} crushing 
+     * @param {Number} zoneOf 
+     * @param {Number} hurling 
+     * @param {Array} hurlingDirections 
+     */
+    constructor(targets, hammering = 0, crushing = false, zoneOf = 0, hurling = 0, hurlingDirections = []) {
+        this.targets = targets || new Set()
+
         this.hammering = hammering
         this.crushing = crushing
         this.zoneOf = zoneOf
         this.hurling = hurling
         this.hurlingDirections = hurlingDirections
 
-        this.consumedPsiPoints = hammering + hurling + crushing + zoneOf
+        this.psiPointsCost = hammering + hurling + (crushing ? 2 : 0) + zoneOf
     }
+
+    performPsionicPower() {
+        if (this.psiPointsCost > psiPointsPerUseLimit) {
+            ui.notifications.warn(`You can spend at most ${psiPointsPerUseLimit} Psi Points at once`)
+            return false
+        }
+
+        const resourceKey = Object.keys(actor.data.data.resources).filter(k => actor.data.data.resources[k].label === "Psi Points").shift();
+        if (resourceKey && (actor.data.data.resources[resourceKey].value < this.psiPointsCost)) {
+            ui.notifications.warn("You don't have enough Psi Points available")
+            return false
+        }
+
+        if (this.targets.size === 0) {
+            ui.notifications.warn("Select at least one target")
+            return false
+        }
+
+        if (this.targets.size > 1 && this.zoneOf === 0) {
+            ui.notifications.warn("You must use the Zone Of modifier to have more than one target")
+            return false
+        }
+
+        for (target of this.targets) {
+            this.rollSave(target)
+        }
+    }
+
+    /**
+     * 
+     * @param {Token} target 
+     */
+    rollSave(target) {
+        target.
+    }
+
 }
 
 const dialog = new Dialog({
     title: 'Telekinetic Force',
     content: `
     <form>
-        <div>Max psi points usable = ${maxPsiPointsExpendableAtOnce}</div>
+        <div>Max psi points usable = ${psiPointsPerUseLimit}</div>
         <label>Hammering</label>
         <input type="number" name="hammering">
     </form>
@@ -31,7 +76,7 @@ const dialog = new Dialog({
             label: `Ok`,
             callback: (html) => {
                 // get values from form and instantiate the attack
-            
+
             }
         }
     },
